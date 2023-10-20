14c1251e6b22f3f2891f62c98c568d3e5a6781fd
chesy47 <chesy47@gmail.com>
2023-10-20 12:24:38 +0300

    Start page.jsx_2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,10 @@
-import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started TAKE MY TAIL
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+        <p>Get started TAKE MY TAIL</p>
       </div>
     </main>
   );
