9efee28d5b248e11b609d07cdc7d99cd9ac3e3f6
chesy47 <chesy47@gmail.com>
2023-10-23 12:27:44 +0300

    Switch dev_2

---
+++ components/header/Header.jsx
@@ -0,0 +1,19 @@
+import '../header/header.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <div>logo</div>
+      <ul>
+        <li>about</li>
+        <li>contact</li>
+        <li>contact 2</li>
+        <li>contact 3</li>
+        <li>for us</li>
+        <li>dogs</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
