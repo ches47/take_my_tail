59f76e2b07bb0f40dec12e5ccd999fca540e7dd0
chesy47 <chesy47@gmail.com>
2023-10-23 12:48:05 +0300

    Switch master

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -7,8 +7,6 @@ const Header = () => {
       <ul>
         <li>about</li>
         <li>contact</li>
-        <li>contact 2</li>
-        <li>contact 3</li>
         <li>for us</li>
         <li>dogs</li>
       </ul>
